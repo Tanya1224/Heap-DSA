3daeacc41e5c78e78ccd58fa00369cdfa2c32b0d
Tanya Gupta <78593756+Tanya1224@users.noreply.github.com>
2022-12-05 21:31:23 +0530

    Add files via upload

---
+++ PrQueueMax.java
@@ -0,0 +1,15 @@
+package HEAP;
+
+public class PrQueueMax {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<Integer>(Collections.reverseOrder());
+        pq.add(10);
+        pq.add(20);
+        pq.add(15);
+        System.out.println(pq.peek());
+        System.out.println(pq.poll());
+        System.out.println(pq.peek());
+        
+
+    }
+}
