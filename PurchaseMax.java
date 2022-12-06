624a0452331fecd302c28fca07c26914d4fc9e2c
Tanya Gupta <78593756+Tanya1224@users.noreply.github.com>
2022-12-06 21:18:45 +0530

    Add files via upload

---
+++ PurchaseMax.java
@@ -0,0 +1,24 @@
+
+package HEAP;
+
+public class PurchaseMax {
+    public static void main(String[] args) {
+        int n=5;
+        int cost[]=new int[]{1,12,5,111,200};
+        int sum=10;
+        PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
+        int res=0;
+        for(int i=0;i<n;i++)
+        pq.add(cost[i]);
+        for(int i=0;i<n;i++){
+            if(pq.peek()<=sum){
+                sum-=pq.poll();
+                res++;
+            }
+            else{
+                break;
+            }
+        }
+        System.out.println(res);
+    }
+}
